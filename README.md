e799a3797b20db0156143764eb9261aa0f6a5452
cm7799 <7799cm@gmail.com>
2019-06-20 01:04:28 +0530

    updated file

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 This is the Turtlebot work that I am working on in team with Pallav in our Summer internship.
+
+References: https://github.com/ROBOTIS-GIT/turtlebot3
+
+            https://github.com/ROBOTIS-GIT/OpenCR
+            
--- src/key_to_twist3.py
+++ src/key_to_twist3.py
@@ -1,73 +1,31 @@
 #!/usr/bin/env python
-'''#initialising the libraries and msgs we want to use 
-### now in this program data will publish at 10 hz also we can set the velocity parameters in the launch file or by command line"
-import rospy
-from std_msgs.msg import String
-from geometry_msgs.msg import Twist
-#########################################
-## initiating the dictionary corresponding to key we press and twist we want that is linear movement of wheels in x direction and angular in z direction 
-
-
-key_mapping = {'f':[0,1],'b':[0,-1],'l':[-1,0],'r':[1,0],'s':[0,0]}
-
-#### callback function that publish the twist corresponding to the key subscribed 
-
-t = None
-g_vel_scales=[0.5,0.5]
-def keys_cb(msg,twist_pub):
- global t
- if len(msg.data) == 0 or not key_mapping.has_key(msg.data):
-   return
- vels = key_mapping[msg.data[0]]
-
- t.angular.z = vels[0] * g_vel_scales[0]
- t.linear.x = vels[1] * g_vel_scales[1]
-
-
-
-# initiating the node ,publisher and subscriber 
-if __name__== '__main__':
-  rospy.init_node('key_to_twist')
-  twist_pub = rospy.Publisher('cmd_vel',Twist,queue_size=1)
-  rospy.Subscriber('keys',String,keys_cb,twist_pub)
-  rate = rospy.Rate(10)
-  t = Twist() # initialise with zero
-  if rospy.has_param('~linear_scale'):
-       g_vel_scales[1] = rospy.get_param('~linear_scale')
-  else:
-       rospy.logwarn("linear scale not provided")
-  if rospy.has_param('~angular_scale'):
-      g_vel_scales[0] = rospy.get_param('~angular_scale')
-  else:
-       rospy.logwarn("angular scale not provided")
-  rate = rospy.Rate(10)
-  while not rospy.is_shutdown():
-    twist_pub.publish(t)
-    rate.sleep()
-'''  
-
+#### initialising the libraries
 import rospy
 from std_msgs.msg import String
 from geometry_msgs.msg import Twist
 
 key_mapping = {'f':[0,1],'b':[0,-1],'l':[-1,0],'r':[1,0],'s':[0,0]}
 g_last_twist = None
-g_vel_scales = [0.1,0.1]
+g_vel_scales = [0.1,0.1] # defining default velocity
+                        
 
 def keys_cb(msg, twist_pub):
-  global g_last_twist, g_vel_scales
-  if len(msg.data) == 0 or not key_mapping.has_key(msg.data[0]):
+   global g_last_twist, g_vel_scales
+   if len(msg.data) == 0 or not key_mapping.has_key(msg.data[0]):
     return # unknown key
-  vels = key_mapping[msg.data[0]]
-  g_last_twist.angular.z = vels[0] * g_vel_scales[0]
-  g_last_twist.linear.x = vels[1] * g_vel_scales[1]
-  twist_pub.publish(g_last_twist)
+
+   vels = key_mapping[msg.data[0]]
+   g_last_twist.angular.z = vels[0] * g_vel_scales[0] # scaling to desired vel
+   g_last_twist.linear.x = vels[1] * g_vel_scales[1]
+   twist_pub.publish(g_last_twist) # subscribing and publishing
+
+
 if __name__ == '__main__':
-  rospy.init_node('keys_to_twist')
-  twist_pub = rospy.Publisher('cmd_vel', Twist, queue_size=1)
-  rospy.Subscriber('keys', String, keys_cb, twist_pub)
+  rospy.init_node('keys_to_twist')# initialising the node
+  twist_pub = rospy.Publisher('cmd_vel', Twist, queue_size=1)#defining publisher
+  rospy.Subscriber('keys', String, keys_cb, twist_pub)# defining subsriber
   g_last_twist = Twist() # initializes to zero
-  if rospy.has_param('~linear_scale'):
+  if rospy.has_param('~linear_scale'): #parameter either from launch file or cl 
     g_vel_scales[1] = rospy.get_param('~linear_scale')
   else:
     rospy.logwarn("linear scale not provided; using %.1f" %\
@@ -77,7 +35,9 @@ if __name__ == '__main__':
   else:
     rospy.logwarn("angular scale not provided; using %.1f" %\
     g_vel_scales[0])
-  rate = rospy.Rate(10)
+  rate = rospy.Rate(10) # publishing data at rate of 10 msgs per sec
+  
+
   while not rospy.is_shutdown():
     twist_pub.publish(g_last_twist)
     rate.sleep()
